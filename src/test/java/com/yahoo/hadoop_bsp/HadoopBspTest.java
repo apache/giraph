e74ad53c3ce15ba7788601ca909d8d9439a0dfbc
Avery Ching <aching@apache.org>
2010-11-09 20:27:53 +0000

    Added new ZooKeeperExt test.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/giraph/trunk@1161817 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/com/yahoo/hadoop_bsp/HadoopBspTest.java
+++ src/test/java/com/yahoo/hadoop_bsp/HadoopBspTest.java
@@ -1,5 +1,13 @@
 package com.yahoo.hadoop_bsp;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,8 +15,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class HadoopBspTest 
-    extends TestCase {
+public class HadoopBspTest extends TestCase {
 	
 	/**
 	 * Sample BSP application.
@@ -19,7 +26,7 @@ public class HadoopBspTest
 	 * @param <E>
 	 * @param <M>
 	 */
-	public final class TestBSP<V, E, M> extends HadoopVertex<V, E, M> {
+	public final class TestBsp<V, E, M> extends HadoopVertex<V, E, M> {
 	    public void compute() {
 	    	if (getSuperstep() > 30) {
 	    		voteToHalt();
@@ -32,24 +39,106 @@ public class HadoopBspTest
      *
      * @param testName name of the test case
      */
-    public HadoopBspTest( String testName ) {
-        super( testName );
+    public HadoopBspTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite(HadoopBspTest.class );
+        return new TestSuite(HadoopBspTest.class);
     }
 
     /**
-     * Rigourous Test :-)
+     * Just instantiate the vertex (all functions are implemented)
      */
-    public void testApp()
-    {
-        TestBSP<Integer, String, String> test = new TestBSP<Integer, String, String>();
+    public void testInstantiateVertex() {
+        TestBsp<Integer, String, String> test = 
+        	new TestBsp<Integer, String, String>();
         System.out.println(test.getSuperstep());
-        assertTrue( true );
+    }
+    
+    /**
+     * Run a sample BSP job locally.
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     * @throws InterruptedException 
+     */
+    public void testBspJob() 
+    	throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        conf.setInt(BspJob.BSP_INITIAL_PROCESSES, 1);
+        conf.setFloat(BspJob.BSP_MIN_PERCENT_RESPONDED, 100.0f);
+        conf.setInt(BspJob.BSP_MIN_PROCESSES, 1);
+        conf.setInt(BspJob.BSP_POLL_ATTEMPTS, 1);
+        conf.setInt(BspJob.BSP_POLL_MSECS, 1000);
+        conf.set(BspJob.BSP_ZOOKEEPER_LIST, "localhost:2221");
+        FileSystem hdfs = FileSystem.get(conf);
+    	BspJob bspJob = new BspJob(conf, "testBspJob");
+       	Path inputPath = new Path("/tmp/testBspJobInput");
+       	Path outputPath = new Path("/tmp/testBspJobOutput");    	
+       	hdfs.delete(inputPath, true);
+    	hdfs.mkdirs(inputPath);
+    	byte[] outputArray = new byte[20];
+    	for (int i = 0; i < 20; ++i) {
+    		if ((i % 2) == 1) {
+    			outputArray[i] = 13;
+    		}
+    		else {
+    			outputArray[i] = (byte) (65 + (i % 26));
+    		}
+    	}
+    	FSDataOutputStream outputStream = 
+    		hdfs.create(new Path(inputPath + "/testFile"), true);
+		outputStream.write(outputArray);
+		outputStream.close();
+    	hdfs.delete(outputPath, true);
+    	FileInputFormat.addInputPath(bspJob, inputPath);
+    	FileOutputFormat.setOutputPath(bspJob, outputPath);
+    	bspJob.run();
+    }
+    
+    /**
+     * Run a sample BSP job locally with multiple mappers.
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     * @throws InterruptedException 
+     */
+    public void tBspJobMultiple() 
+    	throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        conf.setInt(BspJob.BSP_INITIAL_PROCESSES, 2);
+        conf.setFloat(BspJob.BSP_MIN_PERCENT_RESPONDED, 100.0f);
+        conf.setInt(BspJob.BSP_MIN_PROCESSES, 2);
+        conf.setInt(BspJob.BSP_POLL_ATTEMPTS, 1);
+        conf.setInt(BspJob.BSP_POLL_MSECS, 1000);
+        FileSystem hdfs = FileSystem.get(conf);
+    	BspJob bspJob = new BspJob(conf, "testBspJob");
+       	Path inputPath = new Path("/tmp/testBspJobMultipleInput");
+       	Path outputPath = new Path("/tmp/testBspJobMultipleOutput");
+       	hdfs.delete(inputPath, true);
+    	hdfs.mkdirs(inputPath);
+    	byte[] outputArray = new byte[20];
+    	for (int i = 0; i < 20; ++i) {
+    		if ((i % 2) == 1) {
+    			outputArray[i] = 13;
+    		}
+    		else {
+    			outputArray[i] = (byte) (65 + (i % 26));
+    		}
+    	}
+    	FSDataOutputStream outputStream = 
+    		hdfs.create(new Path(inputPath + "/testFile1"), true);
+		outputStream.write(outputArray);
+		outputStream.close();
+		outputStream = 
+    		hdfs.create(new Path(inputPath + "/testFile2"), true);
+		outputStream.write(outputArray);
+		outputStream.close();
+    	hdfs.delete(outputPath, true);
+    	FileInputFormat.addInputPath(bspJob, inputPath);
+    	FileOutputFormat.setOutputPath(bspJob, outputPath);
+    	bspJob.run();
     }
 }
---
+++ src/test/java/com/yahoo/hadoop_bsp/ZooKeeperExtTest.java
@@ -0,0 +1,78 @@
+package com.yahoo.hadoop_bsp;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.json.JSONObject;
+
+import junit.framework.TestCase;
+
+public class ZooKeeperExtTest
+		extends TestCase implements Watcher {
+	/** ZooKeeperExt instance */
+	ZooKeeperExt m_zooKeeperExt;
+
+	public final String BASE_PATH = "/_zooKeeperExtTest"; 
+	public final String FIRST_PATH = "/_first"; 
+	public final String SECOND_PATH = "/_second"; 
+	
+	public void process(WatchedEvent event) {
+		return;
+	}
+	
+	@Override
+	public void setUp() {
+		try {
+			m_zooKeeperExt = 
+				new ZooKeeperExt("localhost:2221", 30*1000, this);
+			m_zooKeeperExt.deleteExt(BASE_PATH, -1, true);
+		} catch (KeeperException.NoNodeException e) {
+			System.out.println("Clean start: No node " + BASE_PATH);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} 
+	}
+	
+	@Override
+	public void tearDown() {
+		try {
+			m_zooKeeperExt.close();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testCreateExt() throws KeeperException, InterruptedException {
+		System.out.println("Created: " + 
+			m_zooKeeperExt.createExt(
+				BASE_PATH + FIRST_PATH, 
+				null, 
+				Ids.OPEN_ACL_UNSAFE, 
+				CreateMode.PERSISTENT, 
+				true));
+		m_zooKeeperExt.delete(BASE_PATH + FIRST_PATH, -1);
+		m_zooKeeperExt.delete(BASE_PATH, -1);
+	}
+
+	public void testDeleteExt() throws KeeperException, InterruptedException {
+		m_zooKeeperExt.create(BASE_PATH,
+							  null,
+							  Ids.OPEN_ACL_UNSAFE,
+							  CreateMode.PERSISTENT);
+		m_zooKeeperExt.create(BASE_PATH + FIRST_PATH,
+				  			  null,
+				  			  Ids.OPEN_ACL_UNSAFE,
+				  			  CreateMode.PERSISTENT);
+		try {
+			m_zooKeeperExt.deleteExt(BASE_PATH, -1, false);
+		} catch (KeeperException.NotEmptyException e) {
+			System.out.println(
+				"Correctly failed to delete since not recursive");
+		}
+		m_zooKeeperExt.deleteExt(BASE_PATH, -1, true);
+	}
+}
