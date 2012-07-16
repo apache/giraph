69dce8a13234891802e353bf28465bef28790056
Avery Ching <aching@apache.org>
2012-07-16 22:21:26 +0000

    GIRAPH-248: Generic IdentityVertex for IO testing (Sean Choi via aching).
    git-svn-id: https://svn.apache.org/repos/asf/giraph/trunk@1362289 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/giraph/examples/IdentityVertex.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.giraph.examples;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.giraph.graph.EdgeListVertex;
+
+import java.util.Iterator;
+
+/**
+ * User applications can subclass IdentityVertex, which
+ * simply prints the results that have been read for testing IO related
+ * jobs under any inputformat
+ *
+ * @param <I> Vertex index value
+ * @param <V> Vertex value
+ * @param <E> Edge value
+ * @param <M> Message value
+ */
+@SuppressWarnings("rawtypes")
+
+public abstract class IdentityVertex<I extends WritableComparable,
+  V extends Writable, E extends Writable, M extends Writable>
+  extends EdgeListVertex<I, V, E, M> {
+
+  @Override
+  public void compute(Iterator<M> msgIterator) {
+    voteToHalt();
+  }
+}
