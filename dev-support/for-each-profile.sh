782e00279caf1f9900cc8bb2179f0322ec56e6b7
Nitay Joffe <nitay@apache.org>
2012-11-21 00:30:00 +0000

    GIRAPH-420: build formats in profiles where it works.
    git-svn-id: https://svn.apache.org/repos/asf/giraph/trunk@1411957 13f79535-47bb-0310-9956-ffa450edef68

--- dev-support/for-each-profile.sh
+++ dev-support/for-each-profile.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 if [[ -z `which xpath 2> /dev/null` ]]; then
-  echo "ERROR: xpath not found in path"
+  echo "ERROR: xpath not found in path, install xpath first"
   exit
 fi
 
+DIR=$(cd "$(dirname "$0")" && pwd)
+TOP_DIR=$DIR/..
+
+function echo_separator {
+  echo "======================"
+}
+
 function parse_profiles {
   all_profiles=""
-  poms=$(find . -name pom.xml)
+  poms=$(find $TOP_DIR -name pom.xml)
   for pom in $poms; do
     # Returns something like:
     #   <id>hadoop_0.20.203</id><id>hadoop_1.0</id><id>hadoop_non_secure</id> ...
@@ -30,19 +37,41 @@ parse_profiles
 
 echo "Running on profiles: $all_profiles"
 
+failed_profile_file=$TOP_DIR/failed-profile.txt
+if [[ -r "$failed_profile_file" ]]; then
+  echo "Reading $failed_profile_file to restart from last run state."
+  echo "Remove that file if you want a fresh run"
+  failed_profile=$(cat $failed_profile_file)
+  run_profile=false
+else
+  run_profile=true
+fi
+
 for profile in $all_profiles; do
-  echo "======================"
-  echo "=== $profile ==="
-  echo "======================"
+  if ! $run_profile; then
+    if [[ "${profile}" == "${failed_profile}" ]]; then
+      run_profile=true
+    fi
+  fi
+  if $run_profile; then
+    echo_separator
+    echo "=== $profile ==="
+    echo_separator
 
-  mvn -P$profile $@
-  result=$?
+    mvn -P$profile $@
+    result=$?
 
-  if [[ $result -ne 0 ]]; then
-    echo "======================"
-    echo "=== Failed on profile: $profile"
-    echo "=== Failed command: mvn -P$profile $@"
-    echo "======================"
-    exit $result
+    if [[ $result -ne 0 ]]; then
+      echo_separator
+      echo "=== Failed on profile: $profile"
+      echo "=== Failed command: mvn -P$profile $@"
+      echo_separator
+      echo $profile > $failed_profile_file
+      exit $result
+    fi
+  else
+    echo_separator
+    echo "=== Skipping profile $profile"
+    echo_separator
   fi
 done
