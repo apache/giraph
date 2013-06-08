ea957c226f07fc4c73bb556bfb51576ce3462ae5
Claudio Martella <claudio@apache.org>
2013-06-08 20:26:39 +0200

    GIRAPH-667: all workers suspended at 'saveVertices' when use out of core

--- giraph-core/src/main/java/org/apache/giraph/worker/BspServiceWorker.java
+++ giraph-core/src/main/java/org/apache/giraph/worker/BspServiceWorker.java
@@ -957,6 +957,7 @@ else[HADOOP_NON_SECURE]*/
                   nextPrintVertices = verticesWritten + 250000;
                 }
               }
+              getPartitionStore().putPartition(partition);
               ++partitionIndex;
             }
             vertexWriter.close(getContext()); // the temp results are saved now
