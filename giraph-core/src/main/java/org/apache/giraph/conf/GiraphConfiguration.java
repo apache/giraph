0a959f4c179dd385f28d2a3a28ae0287caf5b35a
Maja Kabiljo <majakabiljo@fb.com>
2018-11-26 10:15:15 -0800

    Fix DefaultJobProgressTracker when splitMasterWorker=false
    Summary: DefaultJobProgressTracker assumes we are using numWorkers+1 mappers, fix that
    
    Test Plan: Ran a job with splitMasterWorker=false, verified job progress gets printed correctly

--- giraph-core/src/main/java/org/apache/giraph/conf/GiraphConfiguration.java
+++ giraph-core/src/main/java/org/apache/giraph/conf/GiraphConfiguration.java
@@ -671,6 +671,16 @@ public class GiraphConfiguration extends Configuration
   }
 
   /**
+   * How many mappers is job asking for, taking into account whether master
+   * is running on the same mapper as worker or not
+   *
+   * @return How many mappers is job asking for
+   */
+  public final int getMaxMappers() {
+    return getMaxWorkers() + (SPLIT_MASTER_WORKER.get(this) ? 1 : 0);
+  }
+
+  /**
    * Utilize an existing ZooKeeper service.  If this is not set, ZooKeeper
    * will be dynamically started by Giraph for this job.
    *
