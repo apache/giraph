19b7b035dab9a04c79ebb46149ab07366751895a
Maja Kabiljo <majakabiljo@fb.com>
2018-12-05 09:54:55 -0800

    GIRAPH-1215: Make FixedCapacityHeaps work with 0 capacity
    Currently FixedCapacityHeaps throw an exception when they are used with capacity 0, this fixes it.

--- giraph-core/src/main/java/org/apache/giraph/types/heaps/FixedCapacityIntLongMinHeap.java
+++ giraph-core/src/main/java/org/apache/giraph/types/heaps/FixedCapacityIntLongMinHeap.java
@@ -81,7 +81,8 @@ public class FixedCapacityIntLongMinHeap
    * @param value Value
    */
   public void add(int key, long value) {
-    if (size == capacity && compare(keys[0], values[0], key, value) >= 0) {
+    if (capacity == 0 ||
+        (size == capacity && compare(keys[0], values[0], key, value) >= 0)) {
       // If the heap is full and smallest element in it is not smaller
       // than value, do nothing
       return;
